e2e6e48c65fe3a8090458d3b152632f57a47eaec
Andrierlison <andrierlisonsilva8@gmail.com>
2020-09-28 13:37:09 -0300

    Change in interface colors, added the .editorconfig file

--- js/visualizar_bordas.js
+++ js/visualizar_bordas.js
@@ -1,19 +1,24 @@
 function visualizar() {
+  var superiorEsquerdo = document.getElementById("superiorEsquerdo").value;
+  var box = (document.getElementById("box").style.borderTopLeftRadius =
+    superiorEsquerdo + "px");
 
-	var superiorEsquerdo = document.getElementById("superiorEsquerdo").value
-	var caixa = document.getElementById("caixa").style.borderTopLeftRadius = superiorEsquerdo + "px"
+  var superiorDireito = document.getElementById("superiorDireito").value;
+  var box = (document.getElementById("box").style.borderTopRightRadius =
+    superiorDireito + "px");
 
-	var superiorDireito = document.getElementById("superiorDireito").value
-	var caixa = document.getElementById("caixa").style.borderTopRightRadius = superiorDireito + "px"
+  var inferiorDireito = document.getElementById("inferiorDireito").value;
+  var box = (document.getElementById("box").style.borderBottomRightRadius =
+    inferiorDireito + "px");
 
-	var inferiorDireito = document.getElementById("inferiorDireito").value
-	var caixa = document.getElementById("caixa").style.borderBottomRightRadius = inferiorDireito + "px"
+  var inferiorEsquerdo = document.getElementById("inferiorEsquerdo").value;
+  var box = (document.getElementById("box").style.borderBottomLeftRadius =
+    inferiorEsquerdo + "px");
 
-	var inferiorEsquerdo = document.getElementById("inferiorEsquerdo").value
-	var caixa = document.getElementById("caixa").style.borderBottomLeftRadius = inferiorEsquerdo + "px"
-
-	var codigo = document.getElementById("codigo").innerHTML = "border-top-left-radius: " + superiorEsquerdo + "px;" + "<br>" +
-		"border-top-right-radius: " + superiorDireito + "px;" + "<br>" +
-		"border-botton-right-radius: " + inferiorDireito + "px;" + "<br>" +
-		"border-botton-left-radius: " + inferiorEsquerdo + "px;"
+  var codigo = (document.getElementById(
+    "codigo"
+  ).innerHTML = `border-top-left-radius: ${superiorEsquerdo}px;<br>
+  border-top-right-radius: ${superiorDireito}px;<br>
+  border-botton-right-radius: ${inferiorDireito}px;<br>
+  border-botton-left-radius: ${inferiorEsquerdo}px;`);
 }
