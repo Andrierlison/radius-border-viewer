c87d1567471865377f3fd39c2131eec0d48c5c1e
Andrierlison <andrierlisonsilva8@gmail.com>
2021-02-20 01:12:49 -0300

    add option width and height to box

--- js/toView.js
+++ js/toView.js
@@ -1,22 +1,73 @@
-function toView() {
-  let box = document.getElementById("box");
+let code = document.getElementById("code");
+let box = document.getElementById("box");
+let copyButton = document.getElementById("copy-button");
+let hiddenInput = document.getElementById("hidden-input");
 
+code.style.display = "none";
+
+function getInputsValues() {
   let topLeft = document.getElementById("topLeft").value;
-  box.style.borderTopLeftRadius = `${topLeft}px`;
-
   let topRight = document.getElementById("topRight").value;
-  box.style.borderTopRightRadius = `${topRight}px`;
-
   let bottomRight = document.getElementById("bottomRight").value;
-  box.style.borderBottomRightRadius = `${bottomRight}px`;
-
   let bottomLeft = document.getElementById("bottomLeft").value;
-  box.style.borderBottomLeftRadius = `${bottomLeft}px`;
+  let boxWidth = document.getElementById("inputWidth").value;
+  let boxHeight = document.getElementById("inputHeight").value;
 
-  document.getElementById(
-    "code"
-  ).innerHTML = `border-top-left-radius: ${topLeft}px;<br>
-  border-top-right-radius: ${topRight}px;<br>
-  border-botton-right-radius: ${bottomRight}px;<br>
-  border-botton-left-radius: ${bottomLeft}px;`;
+  changeValues(topLeft, topRight, bottomRight, bottomLeft, boxWidth, boxHeight);
 }
+
+function changeValues(
+  topLeft,
+  topRight,
+  bottomRight,
+  bottomLeft,
+  boxWidth,
+  boxHeight
+) {
+  box.style.borderTopLeftRadius = `${topLeft}px`;
+  box.style.borderTopRightRadius = `${topRight}px`;
+  box.style.borderBottomRightRadius = `${bottomRight}px`;
+  box.style.borderBottomLeftRadius = `${bottomLeft}px`;
+  box.style.width = `${boxWidth}px`;
+  box.style.height = `${boxHeight}px`;
+
+  copyButton.innerText = "Copy";
+
+  showValues(topLeft, topRight, bottomRight, bottomLeft, boxWidth, boxHeight);
+}
+
+function showValues(
+  topLeft,
+  topRight,
+  bottomRight,
+  bottomLeft,
+  boxWidth,
+  boxHeight
+) {
+  code.innerHTML = `
+  <p>width: ${boxWidth}px;<br>
+  <p>height: ${boxHeight}px;<br>
+  <p>border-top-left-radius: ${topLeft}px;</p>
+  <p>border-top-right-radius: ${topRight}px;</p>
+  <p>border-botton-right-radius: ${bottomRight}px;</p>
+  <p>border-botton-left-radius: ${bottomLeft}px;</p>
+  `;
+
+  code.value = code;
+
+  code.style.display = "block";
+}
+
+function copy() {
+  hiddenInput.value = code.innerText;
+
+  let copyText = hiddenInput;
+
+  copyText.select();
+
+  document.execCommand("copy");
+
+  copyButton.innerText = "Copied!";
+}
+
+copyButton.addEventListener("click", copy);
